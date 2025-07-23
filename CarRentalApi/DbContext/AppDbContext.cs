5ba79d815d2743c65e63388bb73bd3e2d9c61a92
ritikshow <ritikbaryons@gmail.com>
2025-07-23 23:43:01 +0530

    booking details

--- CarRentalApi/DbContext/AppDbContext.cs
+++ CarRentalApi/DbContext/AppDbContext.cs
@@ -11,19 +11,20 @@
         public DbSet<Booking> Bookings { get; set; }
         public DbSet<BookingType> BookingTypes { get; set; }
         public DbSet<Contact> Contacts { get; set; }
-        public DbSet<User> Users { get; set; }  
+        public DbSet<User> Users { get; set; }
         public DbSet<Driver> Drivers { get; set; }
         public DbSet<Notification> Notifications { get; set; }
         public DbSet<ConfirmBooking> ConfirmBooking { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-          
+            modelBuilder.Entity<Car>().OwnsOne(c => c.CarBookingDetails);
+
             modelBuilder.Entity<Contact>().OwnsMany(c => c.Addresses, a =>
             {
-                a.WithOwner().HasForeignKey("ContactId"); 
-                a.Property<int>("Id");                    
-                a.HasKey("Id");                           
+                a.WithOwner().HasForeignKey("ContactId");
+                a.Property<int>("Id");
+                a.HasKey("Id");
             });
 
             base.OnModelCreating(modelBuilder);
--- CarRentalApi/Models/car.cs
+++ CarRentalApi/Models/car.cs
@@ -3,19 +3,29 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace CarRentalApi.Models
 {
-    public class Car: GeneralData
+    public class Car : GeneralData
     {
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-        public int CarId { get; set; } 
+        public int CarId { get; set; }
         public string CarName { get; set; }
         public string CarModel { get; set; }
         public string? ImagePath { get; set; }
         public string? Description { get; set; }
+        public carBookingdata? CarBookingDetails { get; set; }
 
         [NotMapped]
         public IFormFile? ImageFile { get; set; }
 
         public bool IsDeleted { get; set; } = false;
+    }
+
+
+    public class carBookingdata
+    {
+        public string? MinKm { get; set; }
+        public string? RatePerKm { get; set; }
+        public string? Rate { get; set; }
+        public string? extraKm { get; set; }
     }
 }
